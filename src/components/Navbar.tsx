0825352778d50dafedc204b5815d8eb40c7f1e0b
PaTuanas <phanhtuan05@gmail.com>
2026-05-17 00:49:11 +0700

    Update Navbar.tsx

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,14 +1,16 @@
 "use client";
 
+import Link from "next/link"; // Import thư viện Link của Next.js
+
 export default function Navbar() {
     const navItems = [
-        { name: "About Me", href: "#about" },
-        { name: "Education", href: "#education" },
-        { name: "Experience", href: "#experience" },
-        { name: "Personal Skill", href: "#skill" },
-        { name: "Brand", href: "#brand" },
-        { name: "Project", href: "#projects" },
-        { name: "Social Content", href: "#social" },
+        { name: "About Me", href: "/#about" },
+        { name: "Education", href: "/#education" },
+        { name: "Experience", href: "/#experience" },
+        { name: "Personal Skill", href: "/#skill" },
+        { name: "Brand", href: "/#brand" },
+        { name: "Project", href: "/projects" },
+        { name: "Social Content", href: "/#social" },
     ];
 
     return (
@@ -16,13 +18,13 @@ export default function Navbar() {
             <div className="w-full mx-auto px-6 h-16 flex items-center justify-center md:justify-end overflow-x-auto no-scrollbar">
                 <nav className="flex gap-6 md:gap-10 whitespace-nowrap px-4">
                     {navItems.map((item) => (
-                        <a
+                        <Link
                             key={item.name}
                             href={item.href}
                             className="text-sm md:text-base text-stone-500 hover:text-pink-600 hover:font-bold transition-all duration-300 tracking-wider"
                         >
                             {item.name}
-                        </a>
+                        </Link>
                     ))}
                 </nav>
             </div>
